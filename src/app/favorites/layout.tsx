ea312a4e4f2610d5e303ce9b22a9198f34571aa9
RIFAT HOSSAIN <mohammadrifat.hossain.rk@gmail.com>
2024-01-27 12:55:31 +0600

    some components have changed

--- src/app/favorites/layout.tsx
+++ src/app/favorites/layout.tsx
@@ -1,16 +1,23 @@
-import React, { Suspense } from 'react'
+import React, { Suspense } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    //write a loading spinner
     const loadingSpinner = (
-        <div className='w-full flex items-center justify-center'>
-            <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
+        <div className="w-full flex items-center justify-center">
+        <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
         </div>
-    )
-    return <Suspense fallback={loadingSpinner}>
-        {children}
-    </Suspense>;
+    );
+    return (
+        <Suspense fallback={loadingSpinner}>
+        <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen flex flex-col justify-center items-center text-white">
+            <h1 className="text-6xl font-bold mb-6">{`We'll be back soon!`}</h1>
+            <p className="text-2xl mb-12 font-light text-center max-w-md">
+            {`We're currently down for scheduled maintenance for this page. We should be back up
+            and running soon! In the meantime, check out some of our other favorites sections.`}
+            </p>
+            {/* <div className="animate-spin inline-block w-24 h-24 border-8 border-solid rounded-full border-current border-r-transparent align-[-0.125em]"></div> */}
+        </div>
+        </Suspense>
+    );
 };
 
-
-export default Layout
+export default Layout;
